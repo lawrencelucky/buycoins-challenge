8fa7a0add3ac4b3044738ea63c2fc27f9f4718e4
lawrencelucky <lawrencelucky1999@gmail.com>
2021-05-27 16:08:39 +0100

    fix: deployin

--- src/js/toggler.js
+++
@@ -1,8 +0,0 @@
-const toggler = document.querySelector('#toggler');
-const mobileDropdown = document.querySelector('.mobile__view--dropdown');
-
-const togglerHandler = () => {
-  mobileDropdown.classList.toggle('open');
-};
-
-toggler.addEventListener('click', togglerHandler);
